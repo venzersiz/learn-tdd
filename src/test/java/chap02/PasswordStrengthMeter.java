5249347a8fa04f7c7daf30c410b26a4e5e884dcf
Krevis <venzersiz@gmail.com>
2023-06-13 07:17:48 +0900

    2. TDD 시작 > TDD 예: 암호 검사기 > 첫 번째 테스트: 모든 규칙을 충족하는 경우

---
+++ src/test/java/chap02/PasswordStrengthMeter.java
@@ -0,0 +1,9 @@
+package chap02;
+
+public class PasswordStrengthMeter {
+
+    public PasswordStrength meter(String s) {
+
+        return PasswordStrength.STRONG;
+    }
+}
---
+++ src/test/java/chap02/PasswordStrengthMeterTest.java
@@ -0,0 +1,19 @@
+package chap02;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class PasswordStrengthMeterTest {
+
+    @Test
+    void meetsAllCriteria_Then_Strong() {
+        PasswordStrengthMeter meter = new PasswordStrengthMeter();
+
+        PasswordStrength result = meter.meter("ab12!@AB");
+        assertEquals(PasswordStrength.STRONG, result);
+
+        PasswordStrength result2 = meter.meter("abc1!Add");
+        assertEquals(PasswordStrength.STRONG, result2);
+    }
+}
