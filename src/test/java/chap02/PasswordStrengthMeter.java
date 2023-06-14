bc753c65ac51fe821a2821e670da7a39eab49ded
Krevis <venzersiz@gmail.com>
2023-06-15 07:42:46 +0900

    2. TDD 시작 > TDD 예: 암호 검사기 > 세 번째 테스트: 숫자를 포함하지 않고 나머지 조건은 충족하는 경우

--- src/test/java/chap02/PasswordStrengthMeter.java
+++ src/test/java/chap02/PasswordStrengthMeter.java
@@ -8,6 +8,21 @@ public class PasswordStrengthMeter {
             return PasswordStrength.NORMAL;
         }
 
+        boolean containsNum = meetsContainingNumberCriteria(s);
+        if (!containsNum) {
+            return PasswordStrength.NORMAL;
+        }
+
         return PasswordStrength.STRONG;
+    }
+
+    private static boolean meetsContainingNumberCriteria(String s) {
+
+        for (char c : s.toCharArray()) {
+            if ('0' <= c && c <= '9') {
+                return true;
+            }
+        }
+        return false;
     }
 }
