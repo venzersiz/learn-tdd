c19e8bfc7fbb60150718c387e9533e1195ed3ccc
Krevis <venzersiz@gmail.com>
2023-07-26 07:53:57 +0900

    7. 대역 > 모의 객체로 스텁과 스파이 대체

---
+++ src/test/java/chap07/UserRegisterMockTest.java
@@ -0,0 +1,54 @@
+package chap07;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.then;
+import static org.mockito.Mockito.mock;
+
+public class UserRegisterMockTest {
+
+    private UserRegister userRegister;
+    private WeakPasswordChecker mockPasswordChecker = mock(WeakPasswordChecker.class);
+    private MemoryUserRepository fakeRepository = new MemoryUserRepository();
+    private EmailNotifier mockEmailNotifier = mock(EmailNotifier.class);
+
+    @BeforeEach
+    void setUp() {
+        userRegister = new UserRegister(mockPasswordChecker, fakeRepository, mockEmailNotifier);
+    }
+
+    @Test
+    @DisplayName("약한 암호면 가입 실패")
+    void weakPassword() {
+        given(mockPasswordChecker.checkPasswordWeak("pw")).willReturn(true);
+
+        assertThrows(WeakPasswordException.class, () -> userRegister.register("id", "pw", "email"));
+    }
+
+    @Test
+    @DisplayName("회원 가입 시 암호 검사 수행함")
+    void checkPassword() {
+        userRegister.register("id", "pw", "email");
+
+        then(mockPasswordChecker).should().checkPasswordWeak(anyString());
+    }
+
+    @Test
+    @DisplayName("가입하면 메일을 전송함")
+    void whenRegisterThenSendMail() {
+        userRegister.register("id", "pw", "email@email.com");
+
+        ArgumentCaptor<String> captor = ArgumentCaptor.forClass(String.class);
+        then(mockEmailNotifier).should().sendRegisterEmail(captor.capture());
+
+        String realEmail = captor.getValue();
+        assertEquals("email@email.com", realEmail);
+    }
+}
